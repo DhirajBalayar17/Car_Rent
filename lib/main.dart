414b84b494e56b3a81a98b6326a7e195bcc70f2e
Dhiraj Balayar <dhirajbalayar@Dhirajs-MacBook-Air.local>
2025-03-06 21:15:54 +0545

    Signup

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,14 @@ import 'package:my_new/app/app.dart';
 import 'package:my_new/app/di/di.dart';
 import 'package:my_new/core/network/hive_service.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   // Initialize Hive Database
+  //Hive
   await HiveService.init();
+  
 
   // Delete all the hive data and boxes
   // await HiveService().clearAll();
